75e2217263269779001b66222250e8c57e9420d9
onevcat <onevcat@gmail.com>
2022-10-13 13:48:48 +0900

    Initial Commit

---
+++ SPMConfigDemo/SPMConfigDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMConfigDemoApp.swift
+//  SPMConfigDemo
+// 
+//  Created by: onevcat (Wei Wang) on 2022/10/13
+//
+
+import SwiftUI
+
+@main
+struct SPMConfigDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
